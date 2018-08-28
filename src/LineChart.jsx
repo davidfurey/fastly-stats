f510cc6407d663fa82f5c106a1cd8fd680be71a4
David Furey <david.furey@guardian.co.uk>
2018-08-28 14:40:13 +0100

    wip

--- src/LineChart.jsx
+++ src/LineChart.jsx
@@ -1,75 +1,98 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { VictoryChart, VictoryTheme, VictoryLine, VictoryAxis } from 'victory';
-import { fetchStats } from './fastlyapi';
+import { VictoryChart, VictoryTheme, VictoryLine, VictoryLegend } from 'victory';
 
 function LineChart(props) {
 
   return (
-    <div style={{ width: props.width, height: props.height }}>
-      <VictoryChart theme={VictoryTheme.material} scale={{ x: 'time' }} width={props.width} height={props.height}>
-        <VictoryLine
-          style={{
-            data: { stroke: '#c43a31' },
-            parent: { border: '1px solid #ccc' },
-          }}
-          data={props.data}
-        />
-      </VictoryChart>
-    </div>
+    <VictoryLine
+      style={{
+        data: { stroke: '#c43a31' },
+        parent: { border: '1px solid #ccc' },
+      }}
+      data={props.data}
+    />
   );
 }
 
-// export default LineCharts;
+function colours(i: number) {
+  const colourList = [
+    '#e6194b',
+    '#3cb44b',
+    '#ffe119',
+    '#0082c8',
+    '#f58231',
+    '#911eb4',
+    '#46f0f0',
+    '#f032e6',
+    '#d2f53c',
+    '#fabebe',
+    '#008080',
+    '#e6beff',
+    '#aa6e28',
+    '#fffac8',
+    '#800000',
+    '#aaffc3',
+    '#808000',
+    '#ffd8b1',
+    '#000080',
+    '#808080',
+    '#FFFFFF',
+    '#000000',
+  ];
 
-// function mapStateToProps(state) {
-//   return {
-//     data: state.data,
-//   };
-// }
-//
-// export default connect(mapStateToProps)(LineChart);
-
-
-// fetchStats().then((json) => {
-//   console.log(json);
-//   store.dispatch(setData(json.data['1C2vPr3E26cRb4NXa0wMf3'].map((datum) => {
-//     return {
-//       x: new Date(datum.start_time),
-//       y: datum.requests,
-//     };
-//   })));
-// });
+  return colourList[i % colourList.length];
+}
 
 function LineCharts(props) {
 
   let propsData = props.data ? props.data : {};
   let services = props.services ? props.services : new Map();
 
-  console.log('Services');
-  console.log(services);
+  const legendData = Object.entries(propsData).map((service, i) => {
+    return { name: services.get(service[0]), symbol: { fill: colours(i) } }
+  });
+
   return (
     <div>
-      {
-        Object.entries(propsData).map((service) => {
-          console.log(service[0]);
-          const data = service[1].map((datum) => {
-            return {
-              x: new Date(datum.start_time),
-              y: datum.requests,
-            };
-          });
-
-          console.log(service[0]);
-          console.log(services[service[0]]);
-          return (
-            <div>
-              <h1>{services.get(service[0])}</h1>
-              <LineChart width={1200} height={400} data={data} />
-            </div>
-          );
-        })
-      }
+      <div style={{ width: 1600, height: 800, border: '1px solid red' }}>
+        <VictoryChart theme={VictoryTheme.material} y={0} scale={{ x: 'time' }} width={800} height={600}>
+          <VictoryLegend
+            x={800}
+            y={0}
+            title="Legend"
+            centerTitle
+            orientation="vertical"
+            gutter={0}
+            style={{
+              border: { stroke: 'black' },
+              labels: { fontSize: 5 },
+              title: { fontSize: 5 },
+            }}
+            data={legendData}
+          />
+        {
+          Object.entries(propsData).map((service, i) => {
+            const data = service[1].map((datum) => {
+              return {
+                x: new Date(datum.start_time),
+                y: datum.requests,
+              };
+            });
+            return (
+              <VictoryLine
+                key={service[0]}
+                style={{
+                  data: { stroke: colours(i) },
+                  parent: { border: '1px solid #ccc' },
+                }}
+                data={data}
+              />
+            );
+          })
+        }
+        </VictoryChart>
+      </div>
     </div>
   );
 }
